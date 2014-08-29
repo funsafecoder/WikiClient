937a7dab05a5e7bb1de4419f551cecc72e77b3b2
Taylor <runnerbos@gmail.com>
2014-08-29 09:35:48 -0600

    Here's my formatting work

---
+++ src/dataLayer/Page.java
@@ -0,0 +1,90 @@
+package dataLayer;
+
+import org.apache.commons.lang3.text.WordUtils;
+import org.eclipse.mylyn.wikitext.confluence.core.ConfluenceLanguage;
+import org.eclipse.mylyn.wikitext.core.parser.MarkupParser;
+import org.eclipse.mylyn.wikitext.mediawiki.core.MediaWikiLanguage;
+import org.eclipse.mylyn.wikitext.textile.core.TextileLanguage;
+import org.eclipse.mylyn.wikitext.twiki.core.TWikiLanguage;
+
+public class Page 
+{
+	private String pageTitle;
+	private String content;
+	
+	public static void main(String[] args) {
+		System.out.println(new Querying().getPageByTitle("TeleNav"));
+	}
+	
+	public String getPageTitle() {
+		return pageTitle;
+	}
+	
+	private void setPageTitle(String pageTitle) {
+		this.pageTitle = pageTitle;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public String getPlainTextContent(){
+		String filtered = stripBlock(content, "{{", "}}");
+		filtered = stripBlock(filtered, "<ref", "</ref>");
+		filtered = stripBlock(filtered, "<math", "</math>");
+		filtered = stripBlock(filtered, "__", "__");
+		filtered = filtered.replaceAll("(=+?)([^=]+?)\\1", "\n\n$2:\n");
+		filtered = filtered.replaceAll("\\*(.*?)(:?\\*|\n)", "\n •$1");
+		//filtered = filtered.replaceAll("(•(.*)\n)(:? [^( •)])", "$1\n");
+		filtered = filtered.replaceAll("('+)([^=]+?)\\1", "*$2*");
+		filtered = filtered.replaceAll("\\[\\[.*?\\|?(.+?)?\\]\\]", "$1");
+		filtered = filtered.replaceAll("(�€�)|(�€\\?)", "'");
+		filtered = filtered.replaceAll("�„�", "™");
+		filtered = filtered.replaceAll("�€“", "–");
+		filtered = filtered.replaceAll("\n{3,}", "\n\n");
+		System.out.println(filtered);
+		return filtered;
+	}
+	
+	private String stripBlock(String input, String start, String end){
+		int count = 0;
+		String output = "";
+		for(int i = 0; i < input.length(); i++){
+			String spot = input.substring(i, Math.min(Math.max(start.length(), end.length()), input.length()-i)+i);
+			if(spot.startsWith(end) && count > 0){
+				count--;
+				i+=end.length()-1;
+				continue;
+			}
+			if(spot.startsWith(start)) {
+				count++;
+				i+=start.length()-1;
+				continue;
+			}
+			
+			if(count == 0){
+				output += spot.charAt(0);
+			}
+		}
+		return output;
+	}
+	
+	private void setContent(String content) {
+		this.content = content;
+	}
+	
+	public Page(String title, String content)
+	{
+		setPageTitle(title);
+		setContent(content);
+	}
+	
+	@Override
+	public String toString(){
+		String result = "";
+		//result += pageTitle + "\n";
+		
+		result += getPlainTextContent(); //WordUtils.wrap(getPlainTextContent(), 80);
+		return result;
+	}
+}
---
+++ src/dataLayer/Querying.java
@@ -0,0 +1,41 @@
+package dataLayer;
+
+import java.util.ArrayList;
+
+import mongoConnection.MongoIO;
+
+public class Querying 
+{
+	private MongoIO io = new MongoIO("168.63.148.44", 27017, "wiki", "pages");
+	public Boolean CheckTitle(String title)
+	{
+		boolean PageExist = true;
+		if(io.findPage(title).getPageTitle().equals("")){
+			PageExist = false;
+		}
+		return PageExist;
+	}
+	
+	public Page getPageByTitle(String title)
+	{
+		
+		Page page = io.findPage(title);
+		if(title.equals("")){
+			page = null;
+		}
+		return page;
+	}
+	
+	public ArrayList<String> getPagesByKeyword(String keyword)
+	{
+		ArrayList<String> titles = new ArrayList<String>();
+		//query for Pages that have keyword
+		//Page page = current Page queried
+		//titles.add(page.getPageTitle);
+			//Get digest from page.Content 
+				///make sure to print the first time key word appears only
+			//sort so that pages with most references to keyword are first
+
+		return titles;
+	}
+}
